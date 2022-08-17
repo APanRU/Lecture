176377889faf60c18cd8aa76b123034b70299aa3
APanRU <eklmn2004@mail.ru>
2022-08-17 12:01:36 +0300

    add lecture_1_upd

--- Lecture_1/Program.cs
+++ Lecture_1/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, " + username + "!");
